8552c9b23b533ef5cab473d4bfa590150c5ba6fe
edaaslanboga <134918859+edaaslanboga@users.noreply.github.com>
2024-11-19 16:29:55 +0300

    ImmunizationRecord  entity commit edildi

--- Patient.Entities/ImmunizationRecord.cs
+++ Patient.Entities/ImmunizationRecord.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Patient.Entities
 {
-    public class ImmunizationRecord :IEntity
+    public class ImmunizationRecord:IEntity
     {
         public int Id { get; set; } 
         public int PatientID { get; set; }
