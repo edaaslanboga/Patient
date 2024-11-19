095452860906eec0eefd3b1c77ecffba17a15900
edaaslanboga <134918859+edaaslanboga@users.noreply.github.com>
2024-11-19 16:33:56 +0300

    PatientPreferences entity commit edildi

--- Patient.Entities/PatientPreferences.cs
+++ Patient.Entities/PatientPreferences.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Patient.Entities
 {
-    public class PatientPreferences :IEntity
+    public class PatientPreferences:IEntity
     {
         public int Id { get; set; } 
         public int PatientID { get; set; }
