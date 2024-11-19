d20608e3cea7745aad97f78c9394efe898dd24c3
edaaslanboga <134918859+edaaslanboga@users.noreply.github.com>
2024-11-19 16:24:54 +0300

    Doctor entity commit edildi

--- Patient.Entities/Doctor.cs
+++ Patient.Entities/Doctor.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Patient.Entities
 {
-    public class Doctor :IEntity 
+    public class Doctor:IEntity 
     {  public int Id { get; set; }
         [StringLength(50)]
 
