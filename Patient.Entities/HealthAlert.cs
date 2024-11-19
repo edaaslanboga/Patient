07ba8dd78a433dde7736588822a9ff523ae5b898
edaaslanboga <134918859+edaaslanboga@users.noreply.github.com>
2024-11-19 16:27:30 +0300

    HealthAlert entity commit edildi

--- Patient.Entities/HealthAlert.cs
+++ Patient.Entities/HealthAlert.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Patient.Entities
 {
-    public class HealthAlert :IEntity
+    public class HealthAlert:IEntity
     {   
         public int Id { get; set; } 
         public int PatientID { get; set; }
