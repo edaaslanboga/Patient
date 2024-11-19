75c57e4ba7eb709a12e93d4c4ee911eb8f1f60e6
edaaslanboga <134918859+edaaslanboga@users.noreply.github.com>
2024-11-19 16:35:04 +0300

    PPatient entity commit edildi

--- Patient.Entities/PPatient.cs
+++ Patient.Entities/PPatient.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Patient.Entities
 {
-    public class PPatient : IEntity
+    public class PPatient:IEntity
     {
         public int Id { get; set; }
         [StringLength(50)]
